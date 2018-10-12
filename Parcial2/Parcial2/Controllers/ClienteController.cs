30f623d102c47aefe2815824691416c6ca28f219
Alejandro Carvajal <alejandro-carvajal@unilibre.edu.co>
2018-10-12 10:32:17 -0500

    Parcial 2 resuelto

---
+++ Parcial2/Parcial2/Controllers/ClienteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+namespace Parcial2.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Lista()
+        {
+            var Cliente = new Cliente()
+            {
+                Nombre = "Alejandro",
+                Apellido = "Carvajal",
+                Sueldo = 2500000
+                //Crear View
+            };
+            return View(Cliente);
+        }
+    }
+}
---
+++ Parcial2/Parcial2/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class Cliente
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public Decimal Sueldo { get; set; }
+    }
+}
